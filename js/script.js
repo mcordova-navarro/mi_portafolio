06a4f3a360eb2bc694fac9443014b90a71d490df
Milton <milton.cordova@utec.edu.pe>
2025-03-11 23:06:24 -0500

    B

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-// Initialize AOS (Animate On Scroll) library
-AOS.init({
-    duration: 1200,
-    once: true,
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const projects = {
+        multihimnario: ["img/Himnario.jpg", "img/Himnario2.jpg", "img/Himnario3.jpg"],
+        utecgo: ["img/UTEC GO.jpg", "img/UTEC GO2.jpg","img/UTEC GO3.jpg", "img/UTEC GO4.jpg","img/UTEC GO5.jpg", "img/UTEC GO6.jpg","img/UTEC GO7.jpg"],
+        chaveznet: ["img/Net.jpg", "img/Net1.jpg","img/Net2.jpg","img/Net3.jpg","img/Net4.jpg","img/Net5.jpg"],
+        suffixtree: ["img/Paper.jpg", "img/Paper1.jpg","img/Paper2.jpg","img/Paper3.jpg","img/Paper4.jpg"]
+    };
 
-// Smooth scrolling for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+    document.querySelectorAll(".project").forEach((project) => {
+        const projectId = project.getAttribute("data-id");
+        let imgIndex = 0;
+        const imgElement = project.querySelector(".project-image");
+        const prevBtn = project.querySelector(".prev-btn");
+        const nextBtn = project.querySelector(".next-btn");
+
+        function updateImage() {
+            imgElement.src = projects[projectId][imgIndex];
+        }
+
+        prevBtn.addEventListener("click", function () {
+            imgIndex = (imgIndex - 1 + projects[projectId].length) % projects[projectId].length;
+            updateImage();
         });
+
+        nextBtn.addEventListener("click", function () {
+            imgIndex = (imgIndex + 1) % projects[projectId].length;
+            updateImage();
+        });
+
+        updateImage(); // Inicializa con la primera imagen
     });
 });
